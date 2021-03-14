115041f9742d3d2dd6faa492f191683c3d1dddb0
gabozxc <gabozxc@gmail.com>
2021-03-14 18:01:06 -0400

    TD supply 0.1
    First version

---
+++ js/app.js
@@ -0,0 +1,23 @@
+(() => {
+  const sec_categories = document.querySelector(".sec_categories");
+
+  const showProduct = (e) => {
+    if (e.target.classList.contains("controller")) {
+      const id = e.target.getAttribute("id"),
+        query = `.products_row.${id}`;
+
+      document.querySelector(".active").classList.remove("active");
+      e.target.classList.toggle("active");
+
+      document.querySelector(".show").classList.remove("show");
+
+      document.querySelector(query).classList.add("show");
+    }
+  };
+
+
+
+  sec_categories.addEventListener("click", showProduct);
+
+  
+})();
